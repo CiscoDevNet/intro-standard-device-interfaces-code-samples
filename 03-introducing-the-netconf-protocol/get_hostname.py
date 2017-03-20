695b232e61fe1029b997f365f0279858054eb2fc
Hank Preston <hank.preston@gmail.com>
2017-03-20 14:30:04 -0400

    Initial Commit for Code Samples

---
+++ 03-introducing-the-netconf-protocol/get_hostname.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# import the ncclient library
+from ncclient import manager
+import sys
+import xml.dom.minidom
+
+
+# the variables below assume the user is leveraging the
+# DEVNET Sandbox CSR1000v Lab 
+#
+# use the IP address or hostname of your CSR1000V device
+HOST = 'ios-xe-mgmt.cisco.com'
+# use the NETCONF port for your IOS-XE device
+PORT = 10000
+# use the user credentials for your IOS-XE device
+USER = 'root'
+PASS = 'C!sc0123'
+
+
+# create a main() method
+def main():
+    """
+    Main method that retrieves the hostname from config via NETCONF.
+    """
+    with manager.connect(host=HOST, port=PORT, username=USER,
+                         password=PASS, hostkey_verify=False,
+                         device_params={'name': 'default'},
+                         allow_agent=False, look_for_keys=False) as m:
+
+        # XML filter to issue with the get operation
+        hostname_filter = '''
+                          <filter>
+                              <native xmlns="urn:ios">
+                                  <hostname></hostname>
+                              </native>
+                          </filter>
+                          '''
+        result = m.get_config('running', hostname_filter)
+        xml_doc = xml.dom.minidom.parseString(result.xml)
+        hostname = xml_doc.getElementsByTagName("hostname")
+        print(hostname[0].firstChild.nodeValue)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
